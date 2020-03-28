2d5955cce0710cbebe5ebebf97e729fd8a664dac
TheAlterBlack <alterblackoffical@gmail.com>
2020-03-28 12:45:14 +0200

    Add hotel controller and routes

---
+++ controllers/HotelController.js
@@ -0,0 +1,74 @@
+const {Hotel} = require('./../database.js');
+const NotFoundException = require('./../exceptions/NotFoundException');
+const BadRequestException = require('./../exceptions/BadRequestException');
+
+module.exports = {
+    list: async (req, res) => {
+        const data = await Hotel.findAll();
+
+        return res.status(200).send({
+            data,
+        });
+    },
+    item: async (req, res, next) => {
+        const { id } = req.params;
+
+        const item = await Hotel.findByPk(id);
+
+        if (!item) {
+            return next(new NotFoundException());
+        }
+
+        return res.status(200).send({
+            item,
+        });
+    },
+    update: async (req, res, next) => {
+        const { id } = req.params;
+        const data = req.body;
+
+        const item = await Hotel.findByPk(id);
+
+        if (!item) {
+            return next(new NotFoundException());
+        }
+
+        try {
+
+            await item.update(data);
+
+        }catch (error) {
+
+            return next(new BadRequestException(error));
+
+        }
+
+        return res.status(204).send();
+    },
+    delete: async (req, res, next) => {
+        const { id } = req.params;
+        const item = await Hotel.findByPk(id);
+
+        if (!item) {
+            return next(new NotFoundException());
+        }
+
+        await item.destroy();
+
+        return res.status(204).send();
+    },
+    create: async (req, res, next) => {
+        try{
+
+            const data = req.body;
+            await Hotel.create(data);
+
+        }catch(error){
+
+            return next(new BadRequestException(error));
+
+        }
+
+        return res.status(201).send();
+    },
+};
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AuthController = require('./controllers/AuthController');
+const HotelController = require('./controllers/HotelController');
 const auth = require('./middlewares/auth');
 const router = express.Router();
 
@@ -11,5 +12,10 @@ router.get('/', (req, res) => {
 });
 
 router.post('/users/login', AuthController.login);
+router.get('/hotel', auth, HotelController.list);
+router.post('/hotel', auth, HotelController.create);
+router.get('/hotel/:id', auth, HotelController.item);
+router.put('/hotel/:id', auth, HotelController.update);
+router.delete('/hotel/:id', auth, HotelController.delete);
 
 module.exports = router;
